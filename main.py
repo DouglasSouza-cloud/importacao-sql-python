e9ef90b212ae045ad009f5c233fb93c6faebaea6
DouglasSouza-cloud <douglassouza4689@gmail.com>
2026-07-01 21:34:57 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from funcionario import listar_funcionarios
+
+listar_funcionarios()
+
+from funcionario import cadastrar_funcionario
+
+from funcionario import atualizar_cargo
+
+atualizar_cargo(4, "Técnico em Automação")
