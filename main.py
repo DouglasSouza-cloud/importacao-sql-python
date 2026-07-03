36936c931cee66bbd2e3810845e9be76b8f246a7
DOUGLAS DE SOUZA PAIXÃO <douglas_paixao@estudante.sesisenai.org.br>
2026-07-03 15:40:44 -0300

    fix: Organizando banco de dados

--- main.py
+++
@@ -1,9 +0,0 @@
-from funcionario import listar_funcionarios
-
-listar_funcionarios()
-
-from funcionario import cadastrar_funcionario
-
-from funcionario import atualizar_cargo
-
-atualizar_cargo(4, "Técnico em Automação")
